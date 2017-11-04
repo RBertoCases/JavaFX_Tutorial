03e340e7ee9403e8b29a0fb0f03d39c3e3dde12e
RBertoCases <roberto.cases@gmail.com>
2017-11-04 11:37:06 -0700

    added functionality to Delete Button

--- AddressApp/src/ch/makery/address/view/PersonOverviewController.java
+++ AddressApp/src/ch/makery/address/view/PersonOverviewController.java
@@ -97,4 +97,13 @@ private void showPersonDetails(Person person) {
         birthdayLabel.setText("");
         }
     }
+
+    /**
+    * Called when the user clicks on the delete button.
+    */
+    @FXML
+    private void handleDeletePerson() {
+    int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+    personTable.getItems().remove(selectedIndex);
+    }
 }
